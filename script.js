2b4a092501b084c6d0be001d4f04af7c73ba9537
matheusliesenberg <matheusliesenberg3@gmail.com>
2023-12-06 16:43:20 -0300

    update

--- script.js
+++ script.js
@@ -1,21 +1,29 @@
+//A função insert() recebe um número adiciona esse número a da tela da calculadora, o conteúdo da tela é representado pelo elemento HTML com a class dele.
 function insert(num) {
+  //Será adicionado um número a mais ou sinal.
   document.querySelector(".tela").innerHTML += num;
 }
+// Vai apagar os números selecionados da calculadora.
 function clean() {
   document.querySelector(".tela").innerHTML = "";
 }
-function back() {
+function Voltar() {
+  // A função back() remove o último número do conteúdo da tela da calculadora.
   let tela = document.querySelector(".tela").innerHTML;
   document.querySelector(".tela").innerHTML = tela.substring(
     0,
     tela.length - 1
   );
 }
+  //A função calcular() calcula o resultado da expressão matemática que está no conteúdo da tela da calculadora.
 function calcular() {
   let tela = document.querySelector(".tela").innerHTML;
+  // Se tiver comando na tela ele era executar.
   if (tela) {
     document.querySelector(".tela").innerHTML = eval(tela);
+    // Passara para a tela o ressutado.
   } else {
+    // Se nenhuma dos requesitos acima forem atendido o progama avisara que não a comando.
     document.querySelector(".tela").innerHTML = "Sem comando...";
   }
 }
